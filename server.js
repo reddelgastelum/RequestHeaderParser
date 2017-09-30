8b3198c19a6539beb2c7cc95b6f8ab8809d92165
System <none>
2017-09-30 17:38:12 +0000

    Checkpoint

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  var result = {};
+  result.ipaddress = 
+  console.log(request.headers['x-forwarded-for'].split(',')[0]);
+  console.log(request.headers['accept-language'].split(',')[0]);
+  console.log(request.headers['user-agent']);
+  response.end();
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
