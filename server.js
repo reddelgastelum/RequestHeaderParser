604de78b1a15c958d91711c90b623c3eb70af1cb
System <none>
2017-09-30 19:06:12 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -5,22 +5,15 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   var result = {};
-  var str = '';
   result.ipaddress = request.headers['x-forwarded-for'].split(',')[0];
   result.language = request.headers['accept-language'].split(',')[0];
-  var start = request.headers['user-agent'].split('(')[1];
-  console.log(start);
-  result.software = '';
-  console.log(request.headers['user-agent']);
+  result.software = request.headers['user-agent'].split('(')[1].split(')')[0];
   response.send(result);
 });
 
